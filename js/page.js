5009a0f903e7d7519fc3e1d75bf82457b42ccd57
Paulius Kuzmickas <pauliuskuzmickas@Pauliuss-MBP.wireless.dundee.ac.uk>
2018-09-17 20:43:07 +0100

    Small tweaks and bugfixes

--- js/page.js
+++ js/page.js
@@ -23,10 +23,11 @@ var mapController = new MapController(map);
 var poiApi = new WrldPoiApi("428f42677b8a34403e10a5999da0721e");
 var searchbar = new WrldSearchbar("widget-container", map, searchbarConfig);
 var markerController = new WrldMarkerController(map, { searchbar: searchbar });
-//var indoorControl = new WrldIndoorControl("widget-container", map);
 
 // POI views are created in this MarkerController class.
 var localMarkerController = new MarkerController(searchbar, markerController, mapController);
+
+var indoorControl = new WrldIndoorControl("widget-container", map);
 
 searchbar.on('searchresultsupdate', (res) => {
 Object.keys(map._layers).forEach(element => {
@@ -44,7 +45,8 @@ map.indoors.on("indoormapenter", (event) => {
 	poiApi.searchIndoors(event.indoorMap.getIndoorMapId(), 0, (s,r)=>(console.log(r)), {});
 	document.getElementById("map").style.width = "75%";
     document.getElementById("paneldiv").style.width = "25%";
-	document.getElementById("showButton").style.visibility = "hidden";
+  document.getElementById("showButton").style.visibility = "hidden";
+  searchbar.performTagSearch("", false);
 });
 
 map.indoors.on("indoormapexit", (event) =>{
