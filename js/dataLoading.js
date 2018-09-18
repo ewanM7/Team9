b54277eb8eca9670d5bf154415e9b5bfc9b3fa53
AimeeMoffat <AimeeMoffat@computing.dundee.ac.uk>
2018-09-18 14:48:28 +0100

    Displays average, highest and lowest values

--- js/dataLoading.js
+++ js/dataLoading.js
@@ -1,19 +1,17 @@
 src = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"
 
+//Stops Ajax from ating asynchronously.
 $.ajaxSetup({
     async: false
-  });
+});
 
-var url;
+// Gets the data from the JSON file and stores it in an array.
+function getData(name) {
 
-
-function getDataAverage(name) {
-
-    var values = [];
-    var average;
-    
+    var url;
 
     switch (name) {
+
         case "Temperature":
             url = 'Datasets/JSON/temperature.json';
             break;
@@ -34,6 +32,11 @@ function getDataAverage(name) {
             break;
     }
 
+    //console.log(url);
+
+    var values = [];
+    var average;
+
     $.getJSON(url, function (data) {
 
         $.each(data, (key, entry) => {
@@ -43,37 +46,72 @@ function getDataAverage(name) {
                 if (!isNaN(entry2)) {
 
                     values.push(entry2);
+                    //console.log("pushed");
                 }
                 return;
 
 
             })
 
-
-            for (var i = 0; i < values.length; i++) {
-
-            }
-
         });
-       // console.log("length is " + values.length);
 
-
-        var total = 0;
-        for (var i = 0; i < values.length; i++) {
-            total += values[i];
-        }
-        average = total / values.length;
-        average = average.toFixed(2);
-
-
-        //console.log("average is " + average);
-        console.log("average is " + average);
-        return average;
 
     });
-    
-    console.log("average 2 is " + average);
+
+    return values;
+
+}
+
+//Gets the average of the data in the array.
+function getDataAverage(name) {
+
+
+    var values = getData(name);
+    var average;
+
+    var total = 0;
+    for (var i = 0; i < values.length; i++) {
+        total += values[i];
+    }
+    average = total / values.length;
+    average = average.toFixed(2);
+
     return average;
 
 
 }
+
+//Gets the highest value in the array;
+function getHighestValue(name) {
+
+    var values = getData(name);
+
+    for (var i = 0; i < values.length; i++) {
+        //console.log(values[i]);
+    }
+
+    var highest = Math.max(...values);
+    highest = highest.toFixed(2)
+    console.log ("highest is " + highest);
+    
+    return highest;
+   
+
+}
+
+//Gets the lowest value in the array;
+function getLowestValue(name) {
+
+    var values = getData(name);
+
+    for (var i = 0; i < values.length; i++) {
+      //  console.log(values[i]);
+    }
+
+    var lowest = Math.min(...values);
+    lowest = lowest.toFixed(2)
+    console.log ("lowest is " + lowest);
+    return lowest;
+
+}
+
